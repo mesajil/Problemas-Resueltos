af00b247c2b0fb45915b93408c868c13fb952975
Luis Mesajil <lhmesajil@gmail.com>
2022-07-29 19:56:18 -0500

    sets

--- Python/python.py
+++ Python/python.py
@@ -17,11 +17,10 @@ print (cad[5:]) # Rebanada
 print (cad[::-1]) # Retorna cadena invertida
 print (cad)
 
-# Código Ascii
-car = "A"
-print (ord(car)) # CHAR -> ASCII
+# Manejo de caracteres Ascii
+print (ord("A")) # CHAR -> ASCII
 print (chr(65)) # ASCII -> CHAR
-print (chr(ord(car) + 5)) # CHAR -> CHAR
+print (chr(ord("A") + 5)) # CHAR -> CHAR
 
 # Listas
 l = ["Hello ", "World!", 1, 2]
@@ -52,15 +51,43 @@ l.sort(reverse=True) # Ordenar lista de mayor a menor (elementos del mismo tipo)
 print (l)
 
 
-t = () # La tupla es una lista ordenada e inmatuble de datos
-print (t) # tupla vacía
+# Tuples: La tupla es una lista ordenada e inmatuble de datos 
+t = () # tupla vacía
+print (t)
 t = 1, # tupla de un elemento
 print (t)
-t = 5, "Hola", 77 # tupla de varios elementos. También se pueden usar parentesis.
+t = 5, "Hello", 77 # tupla de varios elementos. También se pueden usar parentesis.
 print (t)
 t = tuple([1,2,3,4]) # list -> tuple
 print (t)
 l = list((1,)) # tuple -> list
 print (l)
+
+
+# Sets: Un conjunto es una colección no ordenada objetos únicos. Los objetos son no mutables.
+s = {1, "Hello", False, (1,)} 
+print (s)
+s = set() # Conjunto vacío
+print (s)
+s = set([1,2,2,3]) # list -> set
+print (s)
+s.add(4) # Añadir elemento
+s.discard(1) # Descartar elemento. discard() lanzará excepción si no encuentra el elemento.
+print (s)
+print (2 in s) # Validar elemento
+s.clear() # Descartar todos los elementos
+print (s)
+s = {1,2,3,4,5}
+print (s.pop()) # Descartar elemento aleatorio. Error si el set está vacío
+print (s)
+print ({1, 2} | {2, 3}) # Unión de conjuntos
+print ({1, 2} & {2, 3}) # Interesección de conjuntos
+print ({1, 2} - {2, 3}) # Diferencia de conjuntos
+print ({"Hello", 2} == {2, "Hello"}) # Igualdad de conjuntos
+print ({3,4}.symmetric_difference({4,5})) # Diferencia simétrica
+print ({1,2,3}.issuperset({3,4})) # Validar si un conjunto es superconjunto de otro
+print ({3}.issubset({2,3})) # Validar si un conjunto es subconjunto de otro
+print ({1,2}.isdisjoint({2,3})) # Dos conjuntos son disjuntos si la interseción es el conjunto vacío
+print (frozenset({1,2,3})) # Conjunto inmutable
 
 
