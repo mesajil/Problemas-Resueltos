9cca62128d3df967b5ff67d0f2e4532507c7bc24
Luis Hernán Mesajil Lopez <45523084+mesajil@users.noreply.github.com>
2022-08-11 15:39:49 -0500

    constructor, method

--- Java/pack/Car.java
+++ Java/pack/Car.java
@@ -1,8 +1,18 @@
 package pack;
 
 public class Car {
-    public String color = "red";
+    public String color;
     public static int wheels = 4;
 
+    public Car() {
+        color = "red";
+    }
 
+    public Car(String color_) {
+        color = color_;
+    }
+
+    public void print() {
+        System.out.println("Color: " + color);
+    }
 }
