6dee4aa7fcd6186846cb242f5693c0d6c365c3bf
Luis Mesajil <lhmesajil@gmail.com>
2022-08-12 15:56:43 -0500

    set, get

--- Java/learn/LearnOOP.java
+++ Java/learn/LearnOOP.java
@@ -4,28 +4,23 @@ import pack.Car;
 public class LearnOOP {
     public static void main (String[] args) {
         
-        // Public attributes and methods
+        // Evaluate constructors, attributes and methods 
 
         Car myCar = new Car();
-        System.out.println("color: " + myCar.color);
+        System.out.println("myCar.color: " + myCar.color);
 
-        System.out.println("Changing the color ...");
         myCar.color = "blue";
 
-        System.out.println("color: " + myCar.color);
+        System.out.println("myCar.color: " + myCar.color);
 
-        System.out.println("static wheels: " + myCar.wheels);
-    
-        System.out.println("Setting the color ...");
         myCar.setColor("green");
 
         myCar.printColor();
 
-        // Constructors            
+        myCar = new Car("black");
 
-        System.out.println("new Car( ... ) ...");
-        Car myCar2 = new Car("black");
+        myCar.printColor();
 
-        myCar2.printColor();
+        
     }
 }
--- Java/pack/Car.java
+++ Java/pack/Car.java
@@ -1,22 +1,25 @@
 package pack;
 
 public class Car {
-    public String color;
     public static int wheels = 4;
+    public String color;
 
     public Car() {
         color = "red";
     }
 
-    public Car(String color_) {
-        color = color_;
+    public Car(String newColor) {
+        color = newColor;
     }
 
-    public void setColor(String color_) {
-        color = color_;
+    public void setColor(String newColor) {
+        color = newColor;
     }
-
+    
+    public String getColor () {
+        return color;
+    }
     public void printColor() {
-        System.out.println("Color: " + color);
+        System.out.println("Printing color from the class: " + color);
     }
 }
